80f9673030ba3edf347ad82b747909b95cf6597d
Russell Gordon <rgordon@lcs.on.ca>
2019-10-24 09:18:05 -0400

    Added initial Shopping project

---
+++ Shopping/Shopping/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shopping
+//
+//  Created by Russell Gordon on 2019-10-24.
+//  Copyright © 2019 Russell Gordon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
